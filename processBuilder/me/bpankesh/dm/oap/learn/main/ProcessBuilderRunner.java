ea7a94bdadef64634c1c3223e222a81150955b3f
Pankesh Bamotra <pbamotra@paypal.com>
2014-02-11 14:20:19 +0530

    Process Builder to run external commands from Java code

---
+++ processBuilder/me/bpankesh/dm/oap/learn/main/ProcessBuilderRunner.java
@@ -0,0 +1,24 @@
+package me.bpankesh.dm.oap.learn.main;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import me.bpankesh.dm.oap.learn.util.ProcessBuilderWrapper;
+
+public class ProcessBuilderRunner {
+	public static void main(String[] args) {
+		List<String> cmd = new ArrayList<String>();
+		cmd.add("ls");
+		cmd.add("-al");
+		ProcessBuilderWrapper pbd = null;
+		try {
+			pbd = new ProcessBuilderWrapper(new File("/tmp"), cmd);
+		} catch (Exception e) {
+			System.err.println("Exception occured " + e.getMessage());
+		}
+		System.out.println("Command has terminated with status: " + pbd.getStatus());
+		System.out.println("Output:\n" + pbd.getInfos());
+		System.out.println("Error: " + pbd.getErrors());
+	}
+}
